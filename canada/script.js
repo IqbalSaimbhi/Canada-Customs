6d7f7f0f843ca41f8f3c24e6c16163ddd7b5bcda
IqbalSaimbhi <saimbhi.iqbal@gmail.com>
2020-09-20 21:58:01 -0400

    Commit First

---
+++ canada/script.js
@@ -0,0 +1,5 @@
+$(document).ready(function() {
+  jQuery('.parallax-window').parallax({imageSrc: 'https://d2lm6fxwu08ot6.cloudfront.net/img-thumbs/960w/B6HO7KQH2O.jpg'});
+  jQuery('.parallax-window-two').parallax({imageSrc: 'https://d2lm6fxwu08ot6.cloudfront.net/img-thumbs/960w/WLSFZJ3W9M.jpg'});
+  jQuery('.parallax-window-three').parallax({imageSrc: 'https://d2lm6fxwu08ot6.cloudfront.net/img-thumbs/960w/U3W2SHOLWQ.jpg'});
+  jQuery('.parallax-window-four').parallax({imageSrc: 'https://d2lm6fxwu08ot6.cloudfront.net/img-thumbs/960w/26E6OTLJ7L.jpg'});  });
